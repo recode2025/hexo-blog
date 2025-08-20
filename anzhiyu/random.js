fcdfad924f7dbc9aed498280da3da15865d81db2
recode <recode_2024@qq.com>
2025-08-20 19:30:28 +0800

    Site updated: 2025-08-20 19:30:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/20/basic_sort/","2025/08/20/hello-world/","2025/08/20/dict/","2025/08/20/why_and_how_to_learn/","2025/08/20/search/","2025/08/20/set/"];function toRandomPost(){
+var posts=["2025/08/20/dict/","2025/08/20/set/","2025/08/20/basic_sort/","2025/08/20/search/","2025/08/20/hello-world/","2025/08/20/why_and_how_to_learn/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
